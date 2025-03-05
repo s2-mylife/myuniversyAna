c95eb9e9b87394636e4f6e2ccbb55c7a98b05be3
s2-mylife <tiagolindo305_@hotmail.com>
2025-03-05 20:38:48 -0300

    Update config.js

--- 83de2625c74bf5af34bb9f452666b96ab495eee2/b395f24bce01e66d35ab677a7feb8e99aeadc6f3/config.js
+++ 83de2625c74bf5af34bb9f452666b96ab495eee2/b395f24bce01e66d35ab677a7feb8e99aeadc6f3/config.js
@@ -2,21 +2,9 @@
         // Configuração das estrelas
         const memories = [
             {
-                title: "Primeiro Passo",
-                text: "As palavras podem falhar, mas um gesto de carinho digital fala diretamente ao coração. Surpreenda quem você ama com uma página personalizada que eterniza sentimentos únicos.",
+                title: "Primeiro contato",
+                text: "Queimando meus neuronios para conseguir chamar a atenção da gatinha!!!",
                 images: ["photos/photo1.jpg"],
                 color: "orange"
-            },
-            {
-                title: "Segundo passo",
-                text: "Cada memória compartilhada merece ser celebrada de forma especial. No Afeto Digital, transformamos momentos em declarações digitais inesquecíveis.",
-                images: ["photos/photo2.jpg"],
-                color: "white"
-            },
-            {
-                title: "Ultimo passo",
-                text: "Às vezes, a melhor forma de expressar o que sentimos é com simplicidade e beleza. Deixe-nos criar algo único para tocar o coração de quem você ama.",
-                images: ["photos/photo3.jpg", "photos/photo4.jpg", "photos/photo5.jpg", "photos/photo6.jpg"],
-                color: "white"
             }
         ];
